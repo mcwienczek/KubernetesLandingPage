7c2fc1edaf342756e0f92d5a4e7c0cead70318a3
agnieszkabugla <agnieszka.bugla3@gmail.com>
2018-01-31 11:59:28 +0100

    added email validation

--- public/index.js
+++ public/index.js
@@ -1,49 +1,60 @@
 $(document).ready(function(){
 
     $("button").click(function() {
-        if($("#workshop").is(':checked') || $("#video").is(':checked')) {
+        if($("#workshop").is(':checked') || $("#video").is(':checked')) {           
             var email = $("#email").val();
             var workshop = $("#workshop").is(':checked');
             var video = $("#video").is(':checked');
-    
-            var dataToSend = {
-                email: email,
-                workshop: workshop,
-                video: video
-            };
-            $.ajax({
-                type: "POST",
-                url: "/registration",
-                // The key needs to match your method's input parameter (case-sensitive).
-                data: JSON.stringify(dataToSend),
-                contentType: "application/json",
-                dataType: "json"
-            })
-            .done(function() {
-                $(".thanks").css("display", "block"); 
-                $("button").css("display", "none");
-            })
-            .fail(function(data) {
-                $.confirm({
-                    title: 'Encountered an error!',
-                    content: 'Something went downhill, this may be serious',
-                    type: 'red',
-                    typeAnimated: true,
-                    buttons: {
-                        tryAgain: {
-                            text: 'Try again',
-                            btnClass: 'btn-red',
-                            action: function(){
+            var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            if (filter.test(email) == true) {
+                var dataToSend = {
+                    email: email,
+                    workshop: workshop,
+                    video: video
+                };
+                $.ajax({
+                    type: "POST",
+                    url: "/registration",
+                    // The key needs to match your method's input parameter (case-sensitive).
+                    data: JSON.stringify(dataToSend),
+                    contentType: "application/json",
+                    dataType: "json"
+                })
+                .done(function() {
+                    $(".thanks").css("display", "block"); 
+                    $("button").css("display", "none");
+                })
+                .fail(function(data) {
+                    $.confirm({
+                        title: 'Encountered an error!',
+                        content: 'Something went downhill, this may be serious',
+                        type: 'red',
+                        typeAnimated: true,
+                        buttons: {
+                            tryAgain: {
+                                text: 'Try again',
+                                btnClass: 'btn-red',
+                                action: function(){
+                                }
+                            },
+                            close: function () {
                             }
-                        },
-                        close: function () {
                         }
-                    }
+                    });
+                })
+                .always(function() {
+                    console.log("completed");
                 });
-            })
-            .always(function() {
-                console.log("completed");
-            });
+            } else {
+                $.alert({
+                    title: "Holy guacamole!",
+                    content: 'Your email address is not valid.',
+                    type: 'red', 
+                    typeAnimated: true,
+                    icon: 'fa fa-warning',
+    
+                });
+            }   
         } else {
             $.alert({
                 title: "Holy guacamole!",
