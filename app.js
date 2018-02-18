e8fb3e4dd7f7346d263e2aa9f72c1be9af489e57
Michal Cwienczek <michael.cwienczek@gmail.com>
2018-02-18 17:22:06 +0100

    Removed save to file

--- app.js
+++ app.js
@@ -8,22 +8,10 @@ const express   = require('express'),
 
 app.use(express.static('public'));
 
-//function saving a newly signed email to the file emails.csv
-function saveEmail(dataToWrite) {
-    fs.appendFile('emails.csv', `${dataToWrite.email},${dataToWrite.workshop},${dataToWrite.video}\n`, (err) => {
-    if (err) {
-      console.log('Some error occured - file either not saved or corrupted file saved.');
-    } else {
-      console.log('The new email has been successfully appended to emails.csv!');
-    }
-  });
-}
-
 app.use(bodyParser.json());
 
 app.post('/registration', (req, res) => { 
-    console.log(req.body);
-
+    
     var new_subscriber = {
         "email_address": req.body.email,
         "status": "pending",
@@ -33,11 +21,12 @@ app.post('/registration', (req, res) => {
         }
     };
 
+    console.log(new_subscriber);
+
     //send registration to mailchimp
     axios.post(`https://${process.env.USER}:${process.env.PASSWORD}@us17.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}/members/`, new_subscriber)
     .then(function(success) {
         console.log(`Subscribed email ${req.body.email}`);
-        saveEmail(req.body);
         res.sendStatus(200);
     },
     function(failure){
